db0c88f4276277136ef1984a45166ad7aad1d043
neha-adanur <neha1adanur@gmail.com>
2017-06-12 16:01:35 +0530

    fcommit

---
+++ Assets/ball_move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ball_move : MonoBehaviour {
+
+	public float force;
+	public Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+		rb.AddForce (new Vector2 (1f, 0.5f) * Time.deltaTime * force);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+	Vector2 pos;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		pos = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	float movex=Input.GetAxis ("Horizontal");
+		pos.x += movex * Time.deltaTime * speed;
+		pos.x = Mathf.Clamp (pos.x, -8.4f, +8.4f);
+		transform.position = pos;
+	}
+}
